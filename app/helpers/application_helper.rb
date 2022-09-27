f366bffba06e4efdfb7caaf3330d9054e0b2b6f3
timazarenok <timofey.zarenok@gmail.com>
2022-09-27 07:31:26 +0300

    chore(UI): add header, mainpage and stylesheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def active_class(link_path)
+    current_page?(link_path) ? 'active' : ''
+  end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
