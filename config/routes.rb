53ae0ca7b28be5f7f7b4bbf9c4968b1bd993044e
timazarenok <timofey.zarenok@gmail.com>
2022-09-27 07:31:57 +0300

    chore: add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :events
+  resources :categories
+
+  get 'users/profile', as: 'user_root'
+  devise_for :users
+
+  root to: 'home#index'
 end
